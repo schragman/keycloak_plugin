949df681d03500d3d561ff8be4d80bf85fb7af17
schragman <schragman@gmail.com>
2026-05-14 16:00:03 +0200

    first commit

---
+++ src/main/java/net/schrage/projects/keycloakplugins/KeycloakPluginsApplication.java
@@ -0,0 +1,13 @@
+package net.schrage.projects.keycloakplugins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakPluginsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KeycloakPluginsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/schrage/projects/keycloakplugins/KeycloakPluginsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schrage.projects.keycloakplugins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakPluginsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
